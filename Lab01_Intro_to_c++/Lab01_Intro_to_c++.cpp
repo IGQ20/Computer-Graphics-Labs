9449b4b34ff8f71d60afd44987bb076a5f92469b
IGQ20 <iqbalgutierrezqamar@gmail.com>
2025-03-24 10:33:59 +0000

    Lab 2 Red Triangle Creation.

--- Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
+++ Lab01_Intro_to_c++/Lab01_Intro_to_c++.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World" << std::endl;
-	return 0;
+
+	//degrees to radians conversion
+	float angleInDegrees = 45.0f;
+	float pi = 3.1415927f;
+
+	float angleInRadians = angleInDegrees * pi / 180.0f;
+
+	//output results
+	std::cout << angleInDegrees << "degrees ie equal to" << angleInRadians << "radians." << std::endl;
 }
