bdea7e9852c61a2a7da5e16f52ced7a72540d3b7
IGQ20 <iqbalgutierrezqamar@gmail.com>
2025-03-24 11:31:37 +0000

    Texture Wrapping

--- Lab03_Textures/Lab03_Textures.cpp
+++ Lab03_Textures/Lab03_Textures.cpp
@@ -114,6 +114,19 @@ int main( void )
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
+    /*
+    GL_NEAREST_MIPMAP_NEAREST - uses nearest texture filtering on the nearest mipmap;
+
+    GL_LINEAR_MIPMAP_NEAREST - uses linear texture filtering on the nearest mipmap;
+
+    GL_NEAREST_MIPMAP_LINEAR - uses nearest texture filtering on a linear interpolation between two mipmaps;
+
+    GL_LINEAR_MIPMAP_LINEAR - uses linear texture filtering on a linear interpolation between two mipmaps.
+    */
+    //Texture Wrapping
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
     // Free the image from the memory
     stbi_image_free(data);
 
